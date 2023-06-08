d6eb8370e99b6f6e61789ecb2c719e4835290420
Ganiyy74 <salaudeenabdulganiyy@gmail.com>
2023-06-08 15:49:46 +0400

    personal

---
+++ js/style-switcher.js
@@ -0,0 +1,25 @@
+// //toggle style
+// const styleSwithcerToggler = document.querySelector(".style-switcher-toggler");
+// styleSwithcerToggler.addEventListener("click", () =>{
+//     document.querySelector(".style-switcher").classList.toggle("open");
+// });
+// //hide style swithcher
+// window.addEventListener("scroll", () => {
+//     if(document.querySelector(".style-switcher").classList.contains("open"))
+//     {
+//         document.querySelector(".style-switcher").classList.remove("open");
+//     }
+// })
+// // // theme colors
+//  const alternateStyles = document.querySelectorAll(".alternate-style");
+//  function setActiveStyle(color)
+//  {
+//      alternateStyles.forEach((style) => {
+//          if(color === style.getAttribute("title")){
+//              style.removeAttribute("disabled");
+//       }
+//          else{
+//              style.setAttribute("disable","true");
+//          }
+//      })
+//      }
